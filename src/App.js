f7478f40b93c855314cd2bdfe6f0885704987e9f
Gustavo Rivera Pecunia <grpecunia@gmail.com>
2020-03-06 15:19:15 -0500

    push bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import "./App.css";
-import {Navbar, Nav} from "react-bootstrap"
-
+import { Navbar, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div>
       <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
